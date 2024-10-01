3915bbcf9feeb8b28a9bb25931a4d04dcf42c2b2
balogun abdulmalik <balogunabdulmalik04@gmail.com>
2024-10-01 20:48:21 +0100

    starting specialization with JS(ES6)

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+export default function createReportObject(employeesList) {
+  const report = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
+
+  return report;
+}
+
